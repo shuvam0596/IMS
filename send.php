32eca0267e811663a49c8be56a33a7764a30196f
shuvam gupta <50116050+shuvam0596@users.noreply.github.com>
2019-04-30 01:39:39 +0530

    Delete send.php

--- send.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = 'gourav31ite@gmail.com';
-$subject = 'Test email'; 
-$message = "Hello World!\n\nThis is my first mail."; 
-$headers = "From: gourav31ite@gmail.com\r\nReply-To: gourav31ite@gmail.com";
-$mail_sent = @mail( $to, $subject, $message, $headers );
-echo $mail_sent ? "Mail sent" : "Mail failed";
-?>
